4eed844e1db5e5f7cc45fd3626694d5067435a83
ForgottenGhost <my.name.is.longer.than.yours@hotmail.com>
2016-09-28 23:48:40 +0800

    update

--- Node.py
+++ Node.py
@@ -13,22 +13,14 @@ class Node:
         self.play_times[player_num] += 1
         if win:
             self.win_times[player_num] += 1
-        #print [player_num,self.win_times[player_num],self.play_times[player_num]],self.win_times,self.play_times
         if self.play_times[player_num] != 0 and float(self.win_times[player_num])/float(self.play_times[player_num]) != 0.0:
             self.win_chance[player_num] = float(self.win_times[player_num])/float(self.play_times[player_num])
         else:
             self.win_chance[player_num] = 0.01
 
-
-        #[win_chance[idx] = self.win_times[idx]/self.play_times[idx] for idx in xrange(len(win_chance)) if player_times[idx] != 0 and self.win_times[idx]/self.play_times[idx] != 0.0]
-
     def get_best_move(self):
-        for jdx in xrange(self.size):
-            for idx in xrange(self.size):
-                if self.contain[jdx][idx] != None:
-                    print [idx,jdx],self.contain[jdx][idx].get_win_times(),self.contain[jdx][idx].get_win_chance()
         highest_score = max([max([max([cache_node.get_win_chance()[idx] for cache_node in x_row if cache_node != None])for x_row in self.contain]) for idx in xrange(len(self.win_chance))])
-        returning_list = [[idx,jdx] for kdx in xrange(len(self.win_chance)) for idx in xrange(self.size) for jdx in xrange(self.size) if self.contain[idx][jdx] != None and self.contain[idx][jdx].get_win_chance()[kdx] == highest_score]
+        returning_list = [[jdx,idx] for kdx in xrange(len(self.win_chance)) for idx in xrange(self.size) for jdx in xrange(self.size) if self.contain[idx][jdx] != None and self.contain[idx][jdx].get_win_chance()[kdx] == highest_score]
         return [returning_list[0],highest_score]
 
     def get_win_times(self):
@@ -47,15 +39,16 @@ class Node:
         if self.contain[loc[1]][loc[0]] == None:
             self.contain[loc[1]][loc[0]] = Node(self.size)
 
-    def get_random_node(self,player):
-        #print self.contain
-        #if previous_node != None:
-        #    new_node_pool = [[jdx,idx] for idx in xrange(len(previous_node)) for jdx in xrange(len(previous_node))\
-        #    if previous_node[jdx][idx] != None and self.contain[jdx][idx] == None and [jdx,idx] != previous_loc]
-        #    if len(new_node_pool) != 0:
-        #        new_node_loc = random.choice(new_node_pool)
-        #        self.contain[new_node_loc[0]][new_node_loc[1]].append(Node(self.size))
-        #print [str(node_list) for node_list in self.contain for node in node_list]
+    def get_empty_nodes(self):
+        return [[x,y] for x in xrange(self.size) for y in xrange(self.size) if self.is_empty([x,y])]
+
+    def get_random_node(self,player,previous_node):
+        #print previous_node.get_empty_nodes()
+        empty_nodes = [nodes_loc for nodes_loc in self.get_empty_nodes() if nodes_loc not in previous_node.get_empty_nodes()]
+#        list(set(self.get_empty_nodes()).difference(set(previous_node.get_empty_nodes())))
+        if len(empty_nodes) > 0:
+            loc = random.choice(empty_nodes)
+            self.contain[loc[1]][loc[0]].append(Node(self.size))
         maximum = sum([node.get_win_chance()[int((-0.5*player)+0.5)] for node_list in self.contain for node in node_list if node != None])
         if maximum > 0:
             cumulative_probability = 0.0
@@ -66,13 +59,22 @@ class Node:
                         cumulative_probability += self.contain[y][x].get_win_chance()[int((-0.5*player)+0.5)]
                         if cumulative_probability>=key:
                             return [x,y]
-        loc = [random.randint(0,self.size-1),random.randint(0,self.size-1)]
+#        if self.contain[loc[1]][loc[0]] == None:
+#            self.contain[loc[1]][loc[0]].append(Node(self.size))
+#        return loc
+
+    def test_get_node(self,loc):
         if self.contain[loc[1]][loc[0]] == None:
-            self.contain[loc[1]][loc[0]].append(Node(self.size))
-        return loc
+            self.contain[loc[1]][loc[0]] = Node(self.size)
+        return self.contain[loc[1]][loc[0]]
+
+    def is_empty(self,loc):
+        if self.contain[loc[1]][loc[0]] == None:
+            return True
+        else:
+            return False
 
     def get_node(self,loc):
-        #print loc
         if self.contain[loc[1]][loc[0]] == None:
             self.contain[loc[1]][loc[0]] = Node(self.size)
         return self.contain[loc[1]][loc[0]]
